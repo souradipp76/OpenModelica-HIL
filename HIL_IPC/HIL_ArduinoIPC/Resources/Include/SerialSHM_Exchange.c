2bdc39d77e9582e7e2d16ffcb2709995dffd1dc6
souradip_pal <souradip.piku@gmail.com>
2017-12-27 17:02:07 +0530

    Directory changed.

---
+++ HIL_IPC/HIL_ArduinoIPC/Resources/Include/SerialSHM_Exchange.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdarg.h>
+#include "SerialMI.h"
+
+struct DataExchange {
+    char sendVal[10][32];
+    double getVal[10];
+};
+
+struct DataExchange *PID;
+int fdshm;
+void ShmAccess()
+{   
+    if ((fdshm = shm_open("/MIGOD", O_CREAT | O_RDWR, 0660)) == -1)
+        fprintf(stderr,"Failed to OPEN");
+
+    if ((ftruncate(fdshm, sizeof(struct DataExchange))) == -1)
+        fprintf(stderr,"Failed to TRUNC");
+    if ((PID = mmap(NULL, sizeof(struct DataExchange), PROT_READ | PROT_WRITE, MAP_SHARED, fdshm, 0)) == MAP_FAILED)
+        fprintf(stderr,"Failed to MMAP"); 
+        close(fdshm);   
+}
+
+void ShmWrite(int num1, double tagValue)
+{
+    char outData[]="";
+    ShmAccess();
+    sprintf(outData,"%d,%g\n", num1,tagValue);
+    strcpy(PID-> sendVal[num1], outData);
+    PID-> getVal[num1] = tagValue;
+    //return 0;
+}
+
+char *ShmRead(int num2)
+{
+    ShmAccess();
+    char* retrunVal;
+    retrunVal = PID->sendVal[num2];
+    return retrunVal;
+}		
+
+double SerialToShm(int Port,int S_Baud)
+{
+	char S_Port[32]="";
+	sprintf(S_Port,"/dev/ttyACM%d",Port);
+	serialBegin(S_Port, S_Baud);
+    ShmAccess();
+
+	int i,j;
+	char val[20]="";
+	char someData[32]="";
+	const char* inData;
+    char addr[20]="";
+	if(serialAvailable()>0)
+    {
+            inData = serialRead();
+            strcpy(someData, inData);
+            printf("ardOutput:%s\n", someData);
+            for(i=0; i<strlen(someData); i++)
+            {
+                if(someData[i]==',') 
+                {
+                    addr[i]='\0';
+                    i++;
+                    break;
+                }
+                addr[i] = someData[i];
+            }
+            
+            for(j=i; j<strlen(someData); j++)
+            {
+                val[j-i] = someData[j];
+            }
+            ShmWrite(atoi(addr), atof(val)); 
+    }
+    serialEnd();
+    return 0;
+}
+
+double ShmToSerial(int Port,int S_Baud)
+{
+	char S_Port[32]="";
+	sprintf(S_Port,"/dev/ttyACM%d",Port);
+	serialBegin(S_Port, S_Baud);
+    serialFlush();
+    ShmAccess();
+    int i,j;
+
+	char* outData;
+    for (i=0; i<10; i++)
+    {    
+        outData = ShmRead(i);
+        if(strcmp(outData, "") == 0);
+        else{
+            printf("input:%s", outData);
+            serialWrite(outData);      
+            usleep(1000);
+            strcpy(PID->sendVal[i], "");
+        }
+    }
+    serialEnd();
+    return 0;
+}
---
+++ HIL_IPC/controller/controller.ino
@@ -0,0 +1,36 @@
+
+int refPin = A0;    // select the input pin for the function generator
+
+double Kp = 9.0, Ki = 1.8, Kd = 0.45, Error = 0.0, lastError, Integral = 0.0, Derivative, controlValue, processValue; //variable for PID with default values
+double setPoint = 10.0;
+
+void setup()
+{
+  Serial.begin(115200); //serial begin
+}
+
+void loop()
+{
+  String readAddr = ""; //address String
+  String readVal = ""; //value String
+
+
+  if (Serial.available()) { //when serial data comes from modelica
+    readAddr = Serial.readStringUntil(','); //read Address
+    char readComma = Serial.read(); 
+    readVal = Serial.readStringUntil('\n'); //read value
+
+    processValue = readVal.toDouble(); //change value datatype from string to double
+
+    lastError = Error;
+    //setpoint=analogRead(refPin);
+    Error = setPoint - processValue;
+    Integral = Integral + Error;
+    Derivative = Error - lastError;
+    controlValue = (Kp * Error) + (Ki * Integral) + (Kd * Derivative); //simple PID algorithm
+
+    Serial.print(readAddr + ',' + String(controlValue) + '\n'); //send result serially in the same format as received
+    delay(1);
+
+  }
+}
